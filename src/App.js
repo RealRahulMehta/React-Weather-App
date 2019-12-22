863f0fc9ff3d14f76dcc21088a3af4eb3bad53c8
Rahul Mehta <cruzer.mehta.97@gmail.com>
2019-12-23 01:10:09 +0530

    added svg and result page funtionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
+import Results from "./components/Results";
 import "./App.css";
+import Axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: ""
+      city: "",
+      result: {}
     };
   }
-  showResult = e => {};
+  showResult = e => {
+    if (this.state.city) {
+      Axios.get(
+        `https://www.mapquestapi.com/geocoding/v1/address?key=vf2R1VpfowlzmDyJQy8yAP0YOV2dMH5j&location=${this.state.city}`
+      )
+        .then(res => {
+          if (res.data.results.length) {
+            const lat = res.data.results[0].locations[0].latLng.lat;
+            const lon = res.data.results[0].locations[0].latLng.lng;
+
+            // Proxy to by-pass CORS restrictions
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            // https://api.darksky.net/forecast/key/latitude,longitude
+            Axios.get(
+              proxy +
+                `https://api.darksky.net/forecast/38946ee167f20884649270c4d1eaf870/${lat},${lon}?units=si`
+            )
+              .then(res => this.setState({ result: res.data }))
+              .catch(error => console.log(error));
+          }
+        })
+        .catch(error => console.log(error));
+    }
+  };
   updateSearchedCity = e => {
     this.setState({
       city: e.target.value
@@ -17,7 +43,8 @@ class App extends Component {
   };
   defaultCity = () => {
     this.setState({
-      city: ""
+      city: "",
+      result: {}
     });
   };
   render() {
@@ -29,6 +56,9 @@ class App extends Component {
           defaultCity={this.defaultCity}
           showResult={this.showResult}
         />
+        {Object.keys(this.state.result).length ? (
+          <Results result={this.state.result} city={this.state.city} />
+        ) : null}
       </div>
     );
   }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+const Results = props => {
+  const classes = useStyles();
+  const currently = props.result.currently;
+  const img = currently.icon;
+  const path = require(`../svg/${img}.svg`);
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <img className={classes.image} src={path} alt={img} />
+          <CardContent>
+            <Typography component="h2">{props.city}</Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Temparature: {currently.temperature}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+};
 
-export class Results extends Component {
-  render() {
-    return <div></div>;
+const useStyles = makeStyles({
+  card: {
+    width: 500,
+    height: 400
+  },
+  image: {
+    height: 160
   }
-}
+});
 
 export default Results;
